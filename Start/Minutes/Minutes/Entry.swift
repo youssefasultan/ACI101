cd4d1d43fcb7d1eb0a462b4d230049815b44d319
youssef SULTAN <youssefasultan@gmail.com>
2019-03-10 12:35:06 +0200

    eshta

--- Start/Minutes/Minutes/Entry.swift
+++ Start/Minutes/Minutes/Entry.swift
@@ -2,7 +2,7 @@ import Foundation
 
 class Entry: Codable
 {
-    var title:   String = ""
+    var title:   String = "hello"
     var content: String = ""
     
     var id: String = UUID().uuidString
