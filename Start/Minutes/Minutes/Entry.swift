471f684210c903152dfd757f0b9c7b97eeec182e
youssef SULTAN <youssefasultan@gmail.com>
2019-03-10 12:49:01 +0200

    ok

--- Start/Minutes/Minutes/Entry.swift
+++ Start/Minutes/Minutes/Entry.swift
@@ -3,7 +3,7 @@ import Foundation
 class Entry: Codable
 {
     var title:   String = "hello"
-    var content: String = ""
+    var content: String = "world"
     
     var id: String = UUID().uuidString
     
